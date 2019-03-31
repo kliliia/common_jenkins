1a7810af9d40d7c5140b159513bff0b254e25074
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 22:50:13 -0500

    Fixed steps

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -5,20 +5,17 @@ node('master'){
 
       stage('Check for terraform') {
         env.terraform  = sh returnStdout: true, script: 'terraform --version'
-        sh "echo Terraform already installed version ${env.terraform}" 
+        if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
       }
 
     } catch(er) {
 
     stage ('Install the terraform') {
-        steps {
-
-            sh """
-            wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-            unzip terraform_0.11.11_linux_amd64.zip
-            mv terraform /bin
-            """
-        }
+      sh """
+      wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+      unzip terraform_0.11.11_linux_amd64.zip
+      mv terraform /bin
+      """
     }
   }
 }
