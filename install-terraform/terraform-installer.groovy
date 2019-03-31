059e43c25fe15f923ab7a9091ac6e564ba44f684
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 22:59:15 -0500

    Moved stage

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -1,21 +1,21 @@
 import hudson.EnvVars
 
 node('master'){
+  stage('Install the terraform') {
+
     try {
 
-      stage('Install the terraform') {
-        env.terraform  = sh returnStdout: true, script: 'terraform --version'
-        if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
-      }
+      env.terraform  = sh returnStdout: true, script: 'terraform --version'
+      if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
 
     } catch(er) {
-      
       sh """
       wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
       yum install unzip -y
       unzip terraform_0.11.11_linux_amd64.zip
       mv terraform /bin
       """
+      throw
     }
-
+  }
 }
