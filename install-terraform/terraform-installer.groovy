a1b457191947a38d2eda616db06fa289df0511e6
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 22:45:51 -0500

    Added try catch

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -1,8 +1,21 @@
+import hudson.EnvVars
+
 pipeline {
     agent  any
     stages {
+
+        try {
+
+          stage('Check for terraform') {
+            env.terraform  = sh returnStdout: true, script: 'terraform --version'
+            if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
+          }
+
+        } catch(er) {
+
         stage ('Install the terraform') {
             steps {
+
                 sh """
                 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
                 unzip terraform_0.11.11_linux_amd64.zip
@@ -10,5 +23,6 @@ pipeline {
                 """
             }
         }
+      }
     }
 }
