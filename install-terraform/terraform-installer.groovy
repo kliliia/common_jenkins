753a34ac2487192c23784d6f5cc643bbfe0e4677
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 22:50:59 -0500

    Added install unzip

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -13,6 +13,7 @@ node('master'){
     stage ('Install the terraform') {
       sh """
       wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+      yum install unzip -y
       unzip terraform_0.11.11_linux_amd64.zip
       mv terraform /bin
       """
