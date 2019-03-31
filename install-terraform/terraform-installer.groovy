62417c7a0ef2acae0f6e5fa2b691087576a0208d
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 22:47:01 -0500

    Fixed to run only master

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -1,28 +1,24 @@
 import hudson.EnvVars
 
-pipeline {
-    agent  any
-    stages {
+node('master'){
+    try {
 
-        try {
-
-          stage('Check for terraform') {
-            env.terraform  = sh returnStdout: true, script: 'terraform --version'
-            if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
-          }
-
-        } catch(er) {
-
-        stage ('Install the terraform') {
-            steps {
-
-                sh """
-                wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-                unzip terraform_0.11.11_linux_amd64.zip
-                mv terraform /bin
-                """
-            }
-        }
+      stage('Check for terraform') {
+        env.terraform  = sh returnStdout: true, script: 'terraform --version'
+        if (env.terraform) { sh "echo Terraform already installed version ${env.terraform}" }
       }
+
+    } catch(er) {
+
+    stage ('Install the terraform') {
+        steps {
+
+            sh """
+            wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+            unzip terraform_0.11.11_linux_amd64.zip
+            mv terraform /bin
+            """
+        }
     }
+  }
 }
