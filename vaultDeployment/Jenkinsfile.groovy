55ea90612ab8ab951172c3893858a7bf6296a2c9
Murodbey <abdullayevmurod@gmail.com>
2019-04-22 20:06:19 -0500

    fixed

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -4,7 +4,8 @@ import groovy.json.JsonSlurper
 
 node('master') {
   properties([parameters([
-    booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'),  
+    booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'),
+    booleanParam(defaultValue: false, description: 'Destroy All', name: 'terraformDestroy'),  
     string(defaultValue: 'default_token', description: 'Please provide a token for vault', name: 'vault_token', trim: true),
     string(defaultValue: 'test', description: 'Please provide namespace for vault-deployment', name: 'namespace', trim: true)
     ]
