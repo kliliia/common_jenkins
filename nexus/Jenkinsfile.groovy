7c7d21309a7f4baa0aedebba6ef75ef0f2112178
karakyrt <akiseleichuk84@gmail.com>
2019-04-25 17:32:44 -0500

    :

--- nexus/Jenkinsfile.groovy
+++ nexus/Jenkinsfile.groovy
@@ -7,7 +7,7 @@ node('master') {
     booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'), 
     booleanParam(defaultValue: false, description: 'Destroy All', name: 'terraformDestroy'), 
     string(defaultValue: 'test', description: 'Please provide namespace for nexus-deployment', name: 'namespace', trim: true),
-    string(defaultValue: 'test-message', description 'Please provide the channel name for slack', name: 'channel', trim: true)
+    string(defaultValue: 'test-message', description: 'Please provide the channel name for slack', name: 'channel', trim: true)
     ]
     )])
     stage('Checkout SCM') {
