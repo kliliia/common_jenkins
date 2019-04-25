9ee9bb0e8c2664dc8ae10d9aef317fbed58d1f7b
karakyrt <akiseleichuk84@gmail.com>
2019-04-25 16:38:20 -0500

    changed2

--- nexus/Jenkinsfile.groovy
+++ nexus/Jenkinsfile.groovy
@@ -15,7 +15,7 @@ node('master') {
     stage("Sending slack notification")
     {
       withCredentials([string(credentialsId: 'slack-token', variable: 'SLACK_TOKEN')]) {
-      slackSend baseUrl: 'https://fuchicorp.slack.com/services/hooks/jenkins-ci/', channel: 'test-message', message: 'The nexus job is build success ', token : '$SLACK_TOKEN'
+      slackSend baseUrl: 'https://fuchicorp.slack.com/services/hooks/jenkins-ci/', channel: 'test-message', message: 'The nexus job is build success ', tokenCredentialId: '$SLACK_TOKEN'
       }
     }
     stage('Generate Vars') {
