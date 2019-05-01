00b8dcd7c653342d7c092802967987928ac0655b
karakyrt <akiseleichuk84@gmail.com>
2019-05-01 18:59:14 -0500

    fies changed

--- jira-deployment/Jenkinsfile.groovy
+++ jira-deployment/Jenkinsfile.groovy
@@ -18,7 +18,7 @@ node('master') {
     }
 
     stage('Generate Vars') {
-        def file = new File("${WORKSPACE}/jira-deployment/jira.tfvars")
+        def file = new File("${WORKSPACE}/jira-deployment")
         file.write """
         namespace             =  "${namespace}"
         """
