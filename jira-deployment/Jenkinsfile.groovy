ebd06004594b90a454153e75726e42c503555934
karakyrt <akiseleichuk84@gmail.com>
2019-05-01 20:18:48 -0500

    files changed

--- jira-deployment/Jenkinsfile.groovy
+++ jira-deployment/Jenkinsfile.groovy
@@ -18,7 +18,7 @@ node('master') {
     }
 
     stage('Generate Vars') {
-        def file = new File("${WORKSPACE}/jira_google/jira.tfvars")
+        def file = new File("${WORKSPACE}/google_jira/jira.tfvars")
         file.write """
         namespace             =  "${namespace}"
         """
