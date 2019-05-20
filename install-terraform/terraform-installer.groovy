7e37c594e4048526ec0c7e98667cd8bdb9d53e2a
neshimbekova <neshimbekova@gmail.com>
2019-05-20 11:18:16 -0500

    stage with helm v 2.11.0 created #34

--- install-terraform/terraform-installer.groovy
+++ install-terraform/terraform-installer.groovy
@@ -21,6 +21,14 @@ node('master'){
          unzip terraform_0.11.11_linux_amd64.zip
          mv terraform /bin
          """
+       stage('Installing Helm') {
+         sh """
+         wget https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz
+         tar -zxvf helm-v2.11.0-linux-amd64.tar.gz
+         mv linux-amd64/helm /usr/local/bin/helm
+         """
+
+       }
 
        }
     }
