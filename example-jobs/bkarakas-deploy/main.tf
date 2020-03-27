649c96367d77f818e1bc7da744816a0857367baf
bkarakas <bkarakasu@gmail.com>
2020-03-26 19:48:25 -0500

    deployment_name  changed

--- example-jobs/bkarakas-deploy/main.tf
+++ example-jobs/bkarakas-deploy/main.tf
@@ -1,7 +1,7 @@
 module "bkarakas_deploy" {
 
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "bkarakas"
+  deployment_name        = "bkarakas-website-deploy"
   deployment_environment = "dev"
   deployment_endpoint    = "${var.deployment_endpoint}"
   deployment_path        = "bkarakas"
