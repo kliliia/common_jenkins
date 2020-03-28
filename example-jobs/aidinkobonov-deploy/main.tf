cec6fb524ca60d8959f72c686c76a3fc247f1494
aidinkobonov <aidinkobonov@gmail.com>
2020-03-27 23:24:34 -0500

    edited main and values file

--- example-jobs/aidinkobonov-deploy/main.tf
+++ example-jobs/aidinkobonov-deploy/main.tf
@@ -2,6 +2,6 @@ module "helm_deploy" {
   source                 = "fuchicorp/chart/helm"
   deployment_name        = "aidinkobonov-deploy"
   deployment_environment = "dev"
-  deployment_endpoint    = "aidinkobonov.fuchicorp.com"
+  deployment_endpoint    = "aidinkobonov-deploy.fuchicorp.com"
   deployment_path        = "aidinkobonov-deploy"
 }
