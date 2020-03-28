c111343bd08246c283e8d09313300a559addd985
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-28 09:22:15 -0500

    main.tf created

---
+++ example-jobs/alisait-deploy/main.tf
@@ -0,0 +1,8 @@
+module "talant_module" {
+
+  source                 = "fuchicorp/chart/helm"
+  deployment_name        = "alisait"
+  deployment_environment = "dev"
+  deployment_endpoint    = "${var.deployment_endpoint}"
+  deployment_path        = "alisahp"
+}
