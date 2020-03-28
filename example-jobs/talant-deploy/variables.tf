86184cebdc047121b19dbd714358bd1375f49f3d
Talant Asan <uchkunfan@gmail.com>
2020-03-28 01:58:38 -0400

    example deployment pipeline  has been generated

---
+++ example-jobs/talant-deploy/variables.tf
@@ -0,0 +1,15 @@
+variable "deployment_endpoint" {
+  default = "talant.spacextech.net"
+}
+
+variable "docker_address" {
+  default = "docker.spacextech.net"
+}
+
+variable "image_tag" {
+  default = "0.1"
+}
+
+variable "docker_cred_name" {
+  default = "nexus-creds-talant"
+}
