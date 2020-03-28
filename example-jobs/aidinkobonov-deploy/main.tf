8fb5545eee6a8863e58b3d006066df93580c5ebe
aidinkobonov <aidinkobonov@gmail.com>
2020-03-28 13:43:10 -0500

    edited main.tf file and added variable for endpoin

--- example-jobs/aidinkobonov-deploy/main.tf
+++ example-jobs/aidinkobonov-deploy/main.tf
@@ -2,6 +2,6 @@ module "helm_deploy" {
   source                 = "fuchicorp/chart/helm"
   deployment_name        = "aidinkobonov-deploy"
   deployment_environment = "dev"
-  deployment_endpoint    = "aidinkobonov-deploy.fuchicorp.com"
+  deployment_endpoint    = "${var.deployment_endpoint}"
   deployment_path        = "aidinkobonov-deploy"
 }
