35bf3283e43bfb59491f77d7a5b69895f1df93cf
csamatov <50221223+csamatov96@users.noreply.github.com>
2020-03-29 12:31:27 -0500

    Update main.tf

--- example-jobs/csamatov96-deploy/main.tf
+++ example-jobs/csamatov96-deploy/main.tf
@@ -1,6 +1,6 @@
 module "helm_deploy" {
   source                 = "fuchicorp/chart/helm"
-  deployment_name        = "csamatov96-deploy"
+  deployment_name        = "csamatov96"
   deployment_environment = "dev"
   deployment_endpoint    = "${var.deployment_endpoint}"
   deployment_path        = "csamatov96-deploy"
